7158bec7aa4733364b9720754dd7cb296d620fb9
thingsdonebymansi <heythisismansi@gmail.com>
2025-01-24 15:46:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/scalerfollowalong/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.scalerfollowalong
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val buttonHot = findViewById<Button>(R.id.btnHot)
+        val buttonCool = findViewById<Button>(R.id.btnCool)
+        val linearLayout = findViewById<LinearLayout>(R.id.main)
+
+        buttonCool.setOnClickListener {
+            linearLayout.setBackgroundColor(getColor(R.color.teal))
+            //Toast.makeText(this, "Cool", Toast.LENGTH_SHORT).show()
+        }
+        buttonHot.setOnClickListener {
+            linearLayout.setBackgroundColor(getColor(R.color.red))
+            //Toast.makeText(this, "Hot", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
